9a65b75212b27ab61f33a8ab5fa0e8a3c5d519f6
Jim Kubicek <jim.kubicek@smule.com>
2012-11-19 17:40:00 -0600

    Cleaned up compiler warnings in ASI

--- Classes/ASIFormDataRequest.m
+++ Classes/ASIFormDataRequest.m
@@ -30,7 +30,7 @@
 #pragma mark utilities
 - (NSString*)encodeURL:(NSString *)string
 {
-	NSString *newString = (__bridge_retained NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (__bridge CFStringRef)string, NULL, CFSTR(":/?#[]@!$ &'()*+,;=\"<>%{}|\\^~`"), CFStringConvertNSStringEncodingToEncoding([self stringEncoding]));
+	NSString *newString = (NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)string, NULL, CFSTR(":/?#[]@!$ &'()*+,;=\"<>%{}|\\^~`"), CFStringConvertNSStringEncodingToEncoding([self stringEncoding]));
 	if (newString) {
 		return newString;
 	}
